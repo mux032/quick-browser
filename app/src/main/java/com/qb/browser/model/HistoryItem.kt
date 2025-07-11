a9ea4fdb989de6ebce8f7585aeefd6d12d1df2d7
Mukesh Makwana <mux032@gmail.com>
2025-07-11 19:46:05 +0530

    Merge pull request #5
    * history view updated
    
    * history view updated - added delete button
    
    * Fix: Improve History Activity UX and Functionality
    
    * fixed img url and favicon url extractor
    
    * removed unnecessary logs
    
    * improvement
    
    * improved ui for search bar in history

---
+++ app/src/main/java/com/qb/browser/model/HistoryItem.kt
@@ -0,0 +1,9 @@
+package com.qb.browser.model
+
+/**
+ * Represents different types of items in the history list
+ */
+sealed class HistoryItem {
+    data class Header(val title: String) : HistoryItem()
+    data class WebPageItem(val webPage: WebPage) : HistoryItem()
+}
--- app/src/main/java/com/qb/browser/viewmodel/HistoryViewModel.kt
+++ app/src/main/java/com/qb/browser/viewmodel/HistoryViewModel.kt
@@ -10,6 +10,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.Calendar
 import javax.inject.Inject
 
 @HiltViewModel
@@ -65,5 +66,84 @@ class HistoryViewModel @Inject constructor(
      */
     fun getMostVisitedPages(limit: Int = 10): LiveData<List<WebPage>> {
         return webPageDao.getMostVisitedPages(limit)
+    }
+    
+    /**
+     * Get today's pages
+     */
+    fun getTodayPages(): LiveData<List<WebPage>> {
+        return webPageDao.getTodayPages(getStartOfDay())
+    }
+    
+    /**
+     * Get this week's pages
+     */
+    fun getThisWeekPages(): LiveData<List<WebPage>> {
+        return webPageDao.getThisWeekPages(getStartOfWeek(), getStartOfDay())
+    }
+    
+    /**
+     * Get older pages
+     */
+    fun getOlderPages(): LiveData<List<WebPage>> {
+        return webPageDao.getOlderPages(getStartOfWeek())
+    }
+    
+    /**
+     * Get start of today (midnight)
+     */
+    private fun getStartOfDay(): Long {
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+        return calendar.timeInMillis
+    }
+    
+    /**
+     * Get start of this week (Monday)
+     */
+    private fun getStartOfWeek(): Long {
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+        return calendar.timeInMillis
+    }
+    
+    /**
+     * Get 30 days ago timestamp
+     */
+    private fun getThirtyDaysAgo(): Long {
+        val calendar = Calendar.getInstance()
+        calendar.add(Calendar.DAY_OF_MONTH, -30)
+        return calendar.timeInMillis
+    }
+    
+    /**
+     * Delete today's history (since midnight)
+     */
+    fun deleteTodayHistory() {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                val startOfDay = getStartOfDay()
+                webPageDao.deleteTodayPages(startOfDay)
+            }
+        }
+    }
+    
+    /**
+     * Delete last month's history (last 30 days)
+     */
+    fun deleteLastMonthHistory() {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                val thirtyDaysAgo = getThirtyDaysAgo()
+                webPageDao.deleteLastMonthPages(thirtyDaysAgo)
+            }
+        }
     }
 }
